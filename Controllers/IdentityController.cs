25eacb06a6e83ff9a58f6cd40709cdd2fb85a3b5
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-01-01 18:17:02 +0600

    added creating new user functionality and generated token and confirmation link for the user for email verification

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Identity_Security.Models;
+using Identity_Security.Service;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity_Security.Controllers
 {
     public class IdentityController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender emailSender;
+
+        public IdentityController(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            this.emailSender = emailSender;
+        }
         public async Task<IActionResult> Signup()
         {
             var model = new SignupViewModel();
@@ -16,7 +27,40 @@ namespace Identity_Security.Controllers
         [HttpPost]
         public async Task<IActionResult> Signup(SignupViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                if (await _userManager.FindByEmailAsync(model.Email) != null)
+                {
+                    var user = new IdentityUser
+                    {
+                        Email = model.Email,
+                        UserName = model.Email
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                    user = await _userManager.FindByEmailAsync(user.Email);
+
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    if (result.Succeeded)
+                    {
+                        var confirmationLink = Url.ActionLink("ConfirmEmail", "Identity", new { userId = user.Id, @token = token });
+
+                        await emailSender.SendEmailAsync("info@mydomain.com", user.Email, "Confirm Your email Address", confirmationLink);
+
+                        return RedirectToAction("SignIn");
+                    }
+
+                    ModelState.AddModelError("Signup", string.Join("", result.Errors.Select(x => x.Description)));
+                }
+            }
+
             return View(model);
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            return new OkResult();
         }
 
         public async Task<IActionResult> SignIn()
